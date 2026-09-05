647c68e4f2b81f69a047bdb2b8a03d645ed192ac
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2026-09-05 21:24:50 +0800

    Bedrock got it wrong; embark-consult needs to be loaded afterwards

--- extras/completion.el
+++ extras/completion.el
@@ -91,15 +91,12 @@
          ("M-s L" . consult-line-multi)      ; needed by consult-line to detect isearch
          ))
 
-(use-package embark-consult
-  :ensure t)
-
 ;; Embark: supercharged context-dependent menu; kinda like a
 ;; super-charged right-click.
 (use-package embark
   :ensure t
   :demand t
-  :after (avy embark-consult)
+  :after avy
   :bind (("C-c a" . embark-act))        ; bind this to an easy key to hit
   :init
   ;; Add the option to run embark when using avy
@@ -115,6 +112,11 @@
   ;; After invoking avy-goto-char-timer, hit "." to run embark at the next
   ;; candidate you select
   (setf (alist-get ?. avy-dispatch-alist) 'bedrock/avy-action-embark))
+
+(use-package embark-consult
+  :ensure t
+  :after
+  (embark consult))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
